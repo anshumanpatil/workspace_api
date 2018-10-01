c9ec1fb96ca1d849d2a97045ec53b0af3ee9ed75
Anshuman <anshumanpradippatil1506@gmail.com>
2018-10-01 07:02:31 +0530

    removed bug in register

--- controllers/v1/UserEndpoint.js
+++ controllers/v1/UserEndpoint.js
@@ -1,5 +1,5 @@
 const {User_Master, User_Profile, User_data} = models = require('@models');
-const errorCodes = require('@lib/error-codes')
+const httpCodes = require('@lib/http-codes')
 const constants = require('@lib/constants');
 const errorMessages = require('@lib/error-spells');
 const jwt = require('jsonwebtoken');
@@ -18,12 +18,12 @@ module.exports = class UserEndpoint {
                 raw : true
             }).then(__profile => {
                 if(!__profile){
-                    res.status(errorCodes.NOTFOUND).json({
+                    res.status(httpCodes.NOTFOUND).json({
                         "success": false,
                         "error": errorMessages.PROFILE_NOT_FOUND
                     });
                 }else{
-                    res.status(errorCodes.OK).json({
+                    res.status(httpCodes.OK).json({
                         "success": true,
                         "profile": models.commonMethods.cleanResult(__profile)
                     });
@@ -39,12 +39,12 @@ module.exports = class UserEndpoint {
             raw : true
         }).then(__profile => {
             if(!__profile){
-                res.status(errorCodes.NOTFOUND).json({
+                res.status(httpCodes.NOTFOUND).json({
                     "success": false,
                     "error": errorMessages.PROFILE_NOT_FOUND
                 });
             }else{
-                res.status(errorCodes.OK).json({
+                res.status(httpCodes.OK).json({
                     "success": true,
                     "profile": models.commonMethods.cleanResult(__profile)
                 });
@@ -68,56 +68,51 @@ module.exports = class UserEndpoint {
                     expiresIn: 6000 // expires in 10 min
                 });
                 
-                return res.status(errorCodes.OK).json({
+                return res.status(httpCodes.OK).json({
                     "success" : true,
                     "token" : __token,
                     "user" : __userResult
                 })
             } else if (!user) {
-                return res.status(errorCodes.UNAUTHORIZED).json({
+                return res.status(httpCodes.UNAUTHORIZED).json({
                     "success" : false,
                     "error" : errorMessages.USER_NOT_FOUND
                 })
             } else if (!User_Master.validPassword(req.body.user_password, user.user_password)) {
-                return res.status(errorCodes.UNAUTHORIZED).json({
+                return res.status(httpCodes.UNAUTHORIZED).json({
                     "success" : false,
                     "error" : errorMessages.PASSWORD_ISSMATH
                 })
             } else {
-                return res.status(errorCodes.FORBIDDEN).json(user)
+                return res.status(httpCodes.FORBIDDEN).json(user)
             }
         });
     }
 
     register(req, res) {
         models.sequelize.transaction(function(t) {
-            return User_Master.findOrCreate({
+            return User_Master.findOne({
                 where : {
                     "user_email" : req.body.user_email
                 },
                 raw : true,
                 transaction : t
-            }).spread(function(userResult, created) {
-                let __userResult = {
-                    id : userResult.id,
-                    user_email : userResult.user_email
-                }
-                if (created) {
+            }).then(function(userResult) {
+                if(userResult){
                     return {
-                        success : true,
-                        status: errorCodes.OK,
-                        user : __userResult
-                    };
-                } else {
-                    return {
-                        success : false,
-                        status : errorCodes.CONFLICT,
-                        error : errorMessages.USER_ALREADY_EXISTS,
-                        user : __userResult
-                    };
+                        'success': false,
+                        'user': userResult
+                    }
+                }else{
+                    return User_Master.create(req.body);
                 }
-            }).then(result=>{
-                return res.status(result.status).json(result);
+                
+            }).then(newUser=>{
+                if(newUser.hasOwnProperty('success')){
+                    return res.status(httpCodes.CONFLICT).json(newUser);
+                }else{
+                    return res.status(httpCodes.CREATED).json(newUser);
+                }
             })
         })
     }
