2d60e26eb5512cf612d33951c08cb901d21a7a9c
Anshuman <anshuman.patil@figmd.in>
2018-11-06 17:37:44 +0530

    swagger added in all profile endpoints

--- config/v1/ProfileEndpoint.js
+++ config/v1/ProfileEndpoint.js
@@ -16,19 +16,220 @@ module.exports = {
                     admin: Joi.boolean().allow('').optional(),
                     city: Joi.string().allow('').optional()
                 }
+            },
+            swagger: {
+                "tags": [
+                    "Profile"
+                  ],
+                  "summary": "Create Profile of User",
+                  "description": "",
+                  "operationId": "postProfile",
+                  "consumes": [
+                    "application/x-www-form-urlencoded"
+                  ],
+                  "produces": [
+                    "application/json"
+                  ],
+                  "parameters": [
+                    {
+                      "name": "name",
+                      "in": "formData",
+                      "description": "The first name for user",
+                      "required": false,
+                      "type": "string"
+                    },
+                    {
+                        "name": "middleName",
+                        "in": "formData",
+                        "description": "The first name for user",
+                        "required": false,
+                        "type": "string"
+                    },
+                    {
+                        "name": "surName",
+                        "in": "formData",
+                        "description": "The first name for user",
+                        "required": false,
+                        "type": "string"
+                    },
+                    {
+                        "name": "mobile",
+                        "in": "formData",
+                        "description": "The first name for user",
+                        "required": false,
+                        "type": "string"
+                    },
+                    {
+                        "name": "phone",
+                        "in": "formData",
+                        "description": "The first name for user",
+                        "required": false,
+                        "type": "string"
+                    },
+                    {
+                        "name": "address",
+                        "in": "formData",
+                        "description": "The first name for user",
+                        "required": false,
+                        "type": "string"
+                    },
+                    {
+                        "name": "pincode",
+                        "in": "formData",
+                        "description": "The first name for user",
+                        "required": false,
+                        "type": "string"
+                    },
+                    {
+                        "name": "admin",
+                        "in": "formData",
+                        "description": "The first name for user",
+                        "required": false,
+                        "default": false,
+                        "type": "boolean"
+                    },
+                    {
+                        "name": "city",
+                        "in": "formData",
+                        "description": "The first name for user",
+                        "required": false,
+                        "type": "string"
+                    }
+                  ],
+                  "responses": {
+                    "405": {
+                      "description": "Invalid input"
+                    },
+                    "200": {
+                      "description": "Logged in"
+                    }
+                  },
+                  "security": [{"Authorization": ["write","read"]}]
             }
             
         },
         put: {
             method: 'updateProfile',
             public: false,
-            schema : {}
+            schema : {},
+            swagger: {
+                "tags": [
+                    "Profile"
+                  ],
+                  "summary": "Create Profile of User",
+                  "description": "",
+                  "operationId": "updateProfile",
+                  "consumes": [
+                    "application/x-www-form-urlencoded"
+                  ],
+                  "produces": [
+                    "application/json"
+                  ],
+                  "parameters": [
+                    {
+                      "name": "name",
+                      "in": "formData",
+                      "description": "The first name for user",
+                      "required": false,
+                      "type": "string"
+                    },
+                    {
+                        "name": "middleName",
+                        "in": "formData",
+                        "description": "The first name for user",
+                        "required": false,
+                        "type": "string"
+                    },
+                    {
+                        "name": "surName",
+                        "in": "formData",
+                        "description": "The first name for user",
+                        "required": false,
+                        "type": "string"
+                    },
+                    {
+                        "name": "mobile",
+                        "in": "formData",
+                        "description": "The first name for user",
+                        "required": false,
+                        "type": "string"
+                    },
+                    {
+                        "name": "phone",
+                        "in": "formData",
+                        "description": "The first name for user",
+                        "required": false,
+                        "type": "string"
+                    },
+                    {
+                        "name": "address",
+                        "in": "formData",
+                        "description": "The first name for user",
+                        "required": false,
+                        "type": "string"
+                    },
+                    {
+                        "name": "pincode",
+                        "in": "formData",
+                        "description": "The first name for user",
+                        "required": false,
+                        "type": "string"
+                    },
+                    {
+                        "name": "admin",
+                        "in": "formData",
+                        "description": "The first name for user",
+                        "required": false,
+                        "type": "boolean"
+                    },
+                    {
+                        "name": "city",
+                        "in": "formData",
+                        "description": "The first name for user",
+                        "required": false,
+                        "type": "string"
+                    }
+                  ],
+                  "responses": {
+                    "405": {
+                      "description": "Invalid input"
+                    },
+                    "200": {
+                      "description": "Logged in"
+                    }
+                  },
+                  "security": [{"Authorization": ["write","read"]}]
+            }
             
         },
         get: {
             method: 'getProfile',
             public: false,
-            schema : {}
+            schema : {},
+            swagger: {
+                "tags": [
+                    "Profile"
+                  ],
+                  "summary": "Create Profile of User",
+                  "description": "",
+                  "operationId": "getProfile",
+                  "consumes": [
+                    "application/x-www-form-urlencoded"
+                  ],
+                  "produces": [
+                    "application/json"
+                  ],
+                  "parameters": [],
+                  "responses": {
+                    "405": {
+                      "description": "Invalid input"
+                    },
+                    "200": {
+                      "description": "Logged in"
+                    }
+                  },
+                  "security": [{"Authorization": ["write","read"]}]
+            }
             
         },
         
